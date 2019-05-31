7bf97355a3d1581569a29260392436a78823a062
Vamsi Krishna Bandi <vamsi.bandi@honeywell.com>
2019-05-31 11:15:40 +0530

    hon interview : added API for javascript candidates

---
+++ ConferenceRoomBookingManager/ConferenceRoomBookingManager.API/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using ConferenceRoomBookingManager.DataSource;
+using ConferenceRoomBookingManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConferenceRoomBookingManager.API.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public JsonResult Index()
+        {
+            var buildingConfiguration = GetConfiguration();
+            var rc = new RoomBookingDataClient(buildingConfiguration);
+            var bookingData = rc.GetAllRooms();
+
+            return Json(bookingData, JsonRequestBehavior.AllowGet);
+        }
+
+
+        static BuildingConfiguration GetConfiguration()
+        {
+            return new BuildingConfiguration
+            {
+                BuildingId = Convert.ToInt32(ConfigurationManager.AppSettings["BuildingId"]),
+                MaximumNumberOfFloors = Convert.ToInt32(ConfigurationManager.AppSettings["MaximumNumberOfFloors"]),
+                MinimumNumberOfFloors = Convert.ToInt32(ConfigurationManager.AppSettings["MinimumNumberOfFloors"]),
+                MaximumNumberOfRoomsPerFloor = Convert.ToInt32(ConfigurationManager.AppSettings["MaximumNumberOfRoomsPerFloor"]),
+                MinimumNumberOfRoomsPerFloor = Convert.ToInt32(ConfigurationManager.AppSettings["MinimumNumberOfRoomsPerFloor"]),
+                MaximumNumberOfBookingsPerRoom = Convert.ToInt32(ConfigurationManager.AppSettings["MaximumNumberOfBookingsPerRoom"]),
+                MinimumNumberOfBookingsPerRoom = Convert.ToInt32(ConfigurationManager.AppSettings["MinimumNumberOfBookingsPerRoom"])
+            };
+        }
+    }
+}
